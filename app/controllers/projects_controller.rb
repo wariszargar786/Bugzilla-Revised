3f79a172e05fcb3cc849ce7059a5a4ce274a3938
wariszargar786 <waris.zargar123@gmail.com>
2021-01-05 22:22:47 +0500

    Update user assign to project logic

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,28 +1,33 @@
 class ProjectsController < ApplicationController
   def index
-    user  = User.find(current_user.id)
-    @projects = user.projects.paginate(page:params[:page],per_page:5)
+    user = User.find(current_user.id)
+    @projects = user.projects.paginate(page: params[:page], per_page: 5)
   end
+
   def new
     @project = Project.new
   end
+
   def create
     @project = Project.new(project_params)
     @project['user_id'] = current_user.id
     if @project.save
-      flash[:notice]  = "Project created successfully"
+      flash[:notice] = "Project created successfully"
       redirect_to projects_path
     else
       render 'new'
     end
   end
+
   def show
     @project = Project.friendly.find(params[:id])
     @users = @project.users.all
   end
+
   def edit
     @project = Project.friendly.find(params[:id])
   end
+
   def update
     @project = Project.friendly.find(params[:id])
     if @project.update(project_params)
@@ -34,41 +39,39 @@ class ProjectsController < ApplicationController
 
   def add_user
     @project_id = params[:id]
-    @users = User.where(role: [User.user_role.keys[0],User.user_role.keys[1]])
+    @users = User.where(role: [User.user_role.keys[0], User.user_role.keys[1]])
   end
+
   def add_user_post
-    message =  'User assign to project '
-    anything = params.require(:anything).permit(:user_id,:project_id)
-    if anything
-      project = Project.friendly.find(anything['project_id'])
-      user = User.find(anything['user_id'])
-      status = project.users.exists?
-      if status==false
-        project.users << user
-        message= 'This user already link to this project'
-      end
-      flash[:notice] = message
-      redirect_to projects_path
-    else
-      render :new
+    anything = params.require(:anything).permit(:user_id, :project_id)
+    project = Project.friendly.find(anything['project_id'])
+    user = User.find(anything['user_id'])
+    message = "This User #{user.name} already link to this project"
+    if user.projects.count <= 0
+      user.projects << project
+      message = "User #{user.name} assign to project"
     end
+    flash[:notice] = message
+    redirect_to projects_path
   end
+
   def destroy
     project = Project.friendly.find(params[:id])
     project.users.clear
     project.destroy
     redirect_to projects_path, notice: 'Project deleted successfully'
   end
+
   def delete_user_project
-    user_id =  params[:id]
+    user_id = params[:id]
     user = User.find(user_id)
     user.projects.clear
     redirect_to projects_path, notice: 'User remove successfully ' + user_id
   end
 
-
   private
+
   def project_params
-    params.require(:project).permit(:title , :description)
+    params.require(:project).permit(:title, :description)
   end
 end
