8010315034afcd84e121f9758aba59ec1300b57d
wariszargar786 <waris.zargar123@gmail.com>
2021-01-19 18:50:51 +0500

    Update seeding file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@
 # #
 # # Examples:
 # #
-User.create!([{ name: 'Developer', password:"password", email:"developer@gmail.com", role:1 }, { name: 'Manager', password:"password", email:"manager@gmail.com", role:2 },{ name: 'Quality Assurance', password:"password", email:"qa@gmail.com", role:3 }])
-2.times do |i|
+User.create!([{ name: 'Developer', password:"password", email:"developer@gmail.com", role:1 }, { name: 'Manager', password:"password", email:"waris.zargar123@gmail.com", role:2 },{ name: 'Quality Assurance', password:"password", email:"qa@gmail.com", role:3 }])
+10.times do |i|
   Project.create!(title: "Project # #{i}", description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.")
 end
 @projects = Project.all
@@ -18,5 +18,3 @@ end
   user3 = User.find(3)
   user3.projects << project
 end
-  # Character.create(name: 'Luke', movie: movies.first)
-
